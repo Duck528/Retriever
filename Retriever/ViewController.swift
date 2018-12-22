c83e6a3413270a99867c5c668ab7d75756d150fa
DeockHwan Anh <dhanh0528@gmail.com>
2018-12-22 20:46:32 +0900

    Initial Commit

---
+++ Retriever/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Retriever
+//
+//  Created by thekan on 22/12/2018.
+//  Copyright © 2018 thekan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
