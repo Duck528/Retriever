e83d10bc650d6838deba174aae7863518aec8bff
DeockHwan Anh <dhanh0528@gmail.com>
2018-12-22 23:15:33 +0900

    틀 작업

---
+++ Retriever/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeController.swift
+//  Retriever
+//
+//  Created by thekan on 22/12/2018.
+//  Copyright © 2018 thekan. All rights reserved.
+//
+
+import Cocoa
+import RxSwift
+import RxCocoa
+
+class HomeViewController: NSViewController {
+    
+}
---
+++ Retriever/Home/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  Retriever
+//
+//  Created by thekan on 22/12/2018.
+//  Copyright © 2018 thekan. All rights reserved.
+//
+
+import Foundation
